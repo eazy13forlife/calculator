0f902e6d2917e7d6fbcd0ef6e9cef53d1cbce06c
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-28 04:31:27 -0700

    created layout of calculator

--- source/index.js
+++ source/index.js
@@ -1,1 +1,7 @@
-
+const calculatorBody=document.querySelector(".calculator-body")
+for(let i=0;i<=9;i++){
+  const buttonEl=document.createElement("button");
+  buttonEl.textContent=i;
+  buttonEl.setAttribute("id",`_${i}`);
+  calculatorBody.appendChild(buttonEl);
+}
