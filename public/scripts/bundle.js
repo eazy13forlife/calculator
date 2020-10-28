b0fdc5327c55cc0eef859cc75665ba35fd3d0a1c
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-28 05:10:52 -0700

    newest update

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -15519,15 +15519,22 @@ exports.operate = operate;
 
 var _functions = __webpack_require__(/*! ./functions.js */ "./source/functions.js");
 
+//here we create the calculator body and add all the buttons using a for loop
 var calculatorBody = document.querySelector(".calculator-body");
 for (var i = 0; i <= 9; i++) {
   var buttonEl = document.createElement("button");
   buttonEl.textContent = i;
   buttonEl.setAttribute("id", "_" + i);
+  buttonEl.setAttribute("value", "" + i);
   calculatorBody.appendChild(buttonEl);
 }
 
 console.log((0, _functions.operate)("&", 2, 3, 4));
+
+document.querySelector("#_1").addEventListener("click", function (e) {
+  document.querySelector("#display").value += e.target.value;
+  var number1 = document.querySelector("#display");
+});
 
 /***/ }),
 
--- source/index.js
+++ source/index.js
@@ -1,13 +1,19 @@
 import{add,subtract,divide,multiply,operate} from "./functions.js"
 
-
+//here we create the calculator body and add all the buttons using a for loop
 const calculatorBody=document.querySelector(".calculator-body")
 for(let i=0;i<=9;i++){
   const buttonEl=document.createElement("button");
   buttonEl.textContent=i;
   buttonEl.setAttribute("id",`_${i}`);
+  buttonEl.setAttribute("value",`${i}`)
   calculatorBody.appendChild(buttonEl);
 }
 
 
 console.log(operate("&",2,3,4));
+
+document.querySelector("#_1").addEventListener("click",(e)=>{
+  document.querySelector("#display").value+=e.target.value;
+  const number1=document.querySelector("#display")
+})
