89bb97049250a42105715e511d8786c044448b1b
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-29 08:29:45 -0700

    finished some edge cases

--- source/index.js
+++ source/index.js
@@ -1,6 +1,7 @@
 import{add,subtract,divide,multiply,operate} from "./functions.js"
 
-
+const mik=0;
+console.log(mik.toString())
 
 //here we create the calculator body and add all the buttons using a for loop
 const calculatorBody=document.querySelector(".calculator-body")
@@ -57,30 +58,38 @@ allOperators.forEach((operator)=>{
   operator.addEventListener("click",(e)=>{
     //set the operator equal to the value of the target,in this case, multiply;
     operator=e.target.value;
-    //when i click multiply, create a number variable to hold the displayValue(which we change to a number).
-    const number=+displayValue;
-    //if the first number in numbersArray exists or if it equals 0. if we don't put the second part, it wont work because if first number is 0, that means the first item in array is a falsy value,so the else value will run instead.
-    if(numbersArray[0]||numbersArray[0]===0){
-      //push in the new number value that we just typed, so now we have 2 numbers in our numbersArray along with the operator from previous operator
-      numbersArray.push(number);
-      //call our operate function using 2 arguments. 1. our operator from current numbersArray,which is saved and 2. the numbersArray(which we remove the operator from, so we are only dealing with numbers). The valuu, which is a number type, will be stored in displayValue. Get this value first, push it into an empty array and then push our current operator and save it in this new array.
-      const previousOperator=numbersArray[1];
-      numbersArray.splice(1,1)
-      displayValue=operate(previousOperator,numbersArray);
-      //set the displayEl value equal to whatever displayValue is
-      displayEl.value=displayValue;
-      //create a new array, since we already had 2 numbers in the previous array and got the solution. Remember, this calculator will only work with 2 numbers at a time.
-      numbersArray=[];
-      //push the displayValue into the numbersArray for the first number. Now when we perform another operation, we are operating on this first number.
-      numbersArray.push(displayValue);
+    let number=+displayValue;
+		let letterNumber=displayValue;
+    //if first number exists and operator exists but the second number is empty,yet we're clicking calculator again, just change the operator
+    if((numbersArray[0]||numbersArray[0]===0)&&(numbersArray[1])&&(letterNumber==="")){
+      numbersArray[1]=operator;
+		 //if the first number exists or it equals 0 AND the operator doesn't exist,just push the operator in and that's it. Then we will select another number and after pressing multiply again, the other else statements will run.
+    }else if((numbersArray[0]||numbersArray[0]===0)&&!numbersArray[1]){
       numbersArray.push(operator);
-    //if first number in numbersArray doesn't exist
-    }else if(!numbersArray[0]){
-      //push number into numbersArray, so we have it saved.
-      numbersArray.push(number);
-      //also push operator in the fuction so we have it saved as well
-      numbersArray.push(operator);
-      console.log(numbersArray)
+    }else{
+      //if the first number in numbersArray exists or if it equals 0. if we don't put the second part, it wont work because if first number is 0, that means the first item in array is a falsy value,so the else value will run instead.
+      if(numbersArray[0]||numbersArray[0]===0){
+        //push in the new number value that we just typed, so now we have 2 numbers in our numbersArray along with the operator from previous operator
+        numbersArray.push(number);
+        //call our operate function using 2 arguments. 1. our operator from current numbersArray,which is saved and 2. the numbersArray(which we remove the operator from, so we are only dealing with numbers). The valuu, which is a number type, will be stored in displayValue. Get this value first, push it into an empty array and then push our current operator and save it in this new array.
+        const previousOperator=numbersArray[1];
+        numbersArray.splice(1,1)
+        displayValue=operate(previousOperator,numbersArray);
+        //set the displayEl value equal to whatever displayValue is
+        displayEl.value=displayValue;
+        //create a new array, since we already had 2 numbers in the previous array and got the solution. Remember, this calculator will only work with 2 numbers at a time.
+        numbersArray=[];
+        //push the displayValue into the numbersArray for the first number. Now when we perform another operation, we are operating on this first number.
+        numbersArray.push(displayValue);
+        numbersArray.push(operator);
+      //if first number in numbersArray doesn't exist
+      }else if(!numbersArray[0]){
+        //push number into numbersArray, so we have it saved.
+        numbersArray.push(number);
+        //also push operator in the fuction so we have it saved as well
+        numbersArray.push(operator);
+        console.log(numbersArray)
+      }
     }
     //set displayValue equal to an empty string, so when we click another number, displayValue will add that number to our empty string and then displayEl.value will be this string
     displayValue="";
@@ -89,27 +98,41 @@ allOperators.forEach((operator)=>{
 
 //event listener for equals sign
 document.querySelector("#equals").addEventListener("click",(e)=>{
-  const number=+displayValue;
-  if(numbersArray.length===2){
-    numbersArray.push(number)
-    const currentOperator=numbersArray[1];
-    const newNumbersArray= numbersArray.join("")
-                      .replace(/\D/g," ")
-                      .split(" ")
-                      .filter((number)=>{
-                        if(number!==""){
-                          return true;
-                        }
-                      })
-    newNumbersArray.forEach((number,index)=>{
-      newNumbersArray[index]=+number
-    })
-    console.log(newNumbersArray)
-    displayValue=operate(currentOperator,newNumbersArray);
-    displayEl.value=displayValue;
+  //if array length is 1 meaning something has already been equaled and thats it(but we're clicking equal again) or the length is 0 meaning no operation has been done(but were clicking equal again), don't do anything.
+    if(numbersArray.length===1||numbersArray.length===0){
+
+    }else{
+      //but if the first number and operator has been done, but our second number hasnt been entered,meaning its equal to a blank string or 0(but we're clicking equal again, don't do anything)
+      let number=displayValue;
+      if(number===""){
+
+      }else{
+        number=+displayValue;
+      numbersArray.push(number)
+      const currentOperator=numbersArray[1];
+      const newNumbersArray= numbersArray.join("")
+                        .replace(/\D/g," ")
+                        .split(" ")
+                        .filter((number)=>{
+                          if(number!==""){
+                            return true;
+                          }
+                        })
+      newNumbersArray.forEach((number,index)=>{
+        newNumbersArray[index]=+number
+      })
+      console.log(newNumbersArray)
+      displayValue=operate(currentOperator,newNumbersArray);
+      displayEl.value=displayValue;
+      numbersArray=[];
+      numbersArray.push(displayValue);
+      console.log(numbersArray)
+    }
   }
 })
 
+
+//event listener for the clear button
 document.querySelector("#clear").addEventListener("click",(e)=>{
   numbersArray=[]
   displayValue="";
